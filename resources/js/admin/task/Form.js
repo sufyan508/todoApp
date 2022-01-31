f9217bf479fc90d9560b9896b8b15f082cd1df7c
DESKTOP-TFV6EV0\Rameez <amaannaeem79@gmail.com>
2022-01-31 23:27:52 +0500

    changes of index file

--- resources/js/admin/task/Form.js
+++ resources/js/admin/task/Form.js
@@ -1,11 +1,16 @@
 import AppForm from "../app-components/Form/AppForm";
-import VueIconPicker from "vue-icon-picker";
 import { ColorPicker, ColorPanel } from "one-colorpicker";
+import { fontAwesomePicker } from "font-awesome-picker";
 
 Vue.use(ColorPanel);
 Vue.use(ColorPicker);
 
 Vue.component("task-form", {
+    methods: {
+        yourFunction(selectedIcon) {
+            console.log('selected', selectedIcon)
+        },
+    },
     mixins: [AppForm],
     data: function () {
         return {
@@ -19,4 +24,5 @@ Vue.component("task-form", {
             },
         };
     },
+    "font-awesome-picker": fontAwesomePicker,
 });
--- resources/js/admin/task/index.js
+++ resources/js/admin/task/index.js
@@ -1,7 +1,42 @@
 import "./Listing";
 import "./Form";
 import draggable from "vuedraggable";
-
-components: {
-    draggable;
-}
+let id = 1;
+export default {
+    created() {
+        alert();
+    },
+    name: "simple",
+    display: "Simple",
+    order: 0,
+    components: {
+        draggable,
+    },
+    data() {
+        return {
+            enabled: true,
+            list: [
+                { name: "John", id: 0 },
+                { name: "Joao", id: 1 },
+                { name: "Jean", id: 2 },
+            ],
+            dragging: false,
+        };
+    },
+    computed: {
+        draggingInfo() {
+            return this.dragging ? "under drag" : "";
+        },
+    },
+    methods: {
+        add: function () {
+            this.list.push({ name: "Juan " + id, id: id++ });
+        },
+        replace: function () {
+            this.list = [{ name: "Edgard", id: id++ }];
+        },
+        checkMove: function (e) {
+            window.console.log("Future index: " + e.draggedContext.futureIndex);
+        },
+    },
+};
